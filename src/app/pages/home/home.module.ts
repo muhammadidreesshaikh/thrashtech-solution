f58b78a02dd1b558edf5667e0a49fc9672f4a6bc
Muhammad Idrees <47222718+muhammadidreesshaikh@users.noreply.github.com>
2022-08-14 14:39:14 +0500

    new components added in home init

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -7,11 +7,19 @@ import { SlickCarouselModule } from 'ngx-slick-carousel';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { AboutComponent } from './about/about.component';
+import { ServicesComponent } from './services/services.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    AboutComponent,
+    ServicesComponent,
+    ProjectsComponent,
+    ContactComponent
   ],
   imports: [
     CommonModule,
